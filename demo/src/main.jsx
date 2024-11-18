2986bc7b735ca2eb38ab8ac3a5f5f67334f9bb88
ameer017 <rajiabdullahi907@gmail.com>
2024-11-18 13:32:36 +0100

    radix-ui integrated

--- demo/src/main.jsx
+++ demo/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 
-createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Theme>
+      <App />
+    </Theme>
+  </StrictMode>
+);
